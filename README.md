7c065c4964e784a151f3f9ca04d5c1fbbd1437e2
Daniel Kimsey <daniel.kimsey@hashicorp.com>
2023-04-07 11:35:30 -0500

    Initial complete implementaion

--- README.md
+++ README.md
@@ -4,3 +4,20 @@ No bells, no whistles, just status.
 
 ## Usage
 
+Create a webhook through either [Technique 2](https://github.com/slackapi/slack-github-action#technique-2-slack-app) or [Technique 3](https://github.com/slackapi/slack-github-action#technique-3-slack-incoming-webhook) here. Alternatively, Release engineering may provision one on your behalf for status notifications.
+
+
+```yaml
+steps:
+  # ...
+  - run: |
+      echo "This run failed!" 1>&2
+      exit 1
+  - uses: hashicorp/action-slack-status@v1
+    if: ${{always()}}
+    with:
+        success-message: ":tada: A success message."
+        failure-message: ":boom: A failure message."
+        was-successful: ${{success()}}
+        slack-webhook-url: ${{secrets.slack_webhook_url}}
+```
---
+++ main.js
@@ -0,0 +1,62 @@
+module.exports = ({context, core}) => {
+    let workflowURL = `${context.serverUrl}/${context.repo.owner}/${context.repo.repo}/actions/runs/${context.run_id}`;
+    let status;
+    core.debug(`Computed workflow URL: ${workflowURL}`);
+    try {
+        status = core.getBooleanInput("status");
+    } catch (e) {
+        if (e.name == 'TypeError') {
+            status = core.getInput("status");
+        } else {
+            throw e;
+        }
+    }
+    let color;
+    let message;
+    switch (status) {
+        case 'success':
+        case true:
+            color = "#476ce5"; // blue
+            message = core.getInput("success-message");
+            if (message == "") {
+                core.warning("No success message set, skipping Slack status");
+                return
+            }
+            break;
+        case 'failure':
+        case false:
+            color = "#ed5c5c"; // red
+            message = core.getInput("failure-message");
+            if (message == "") {
+                core.warning("No failure message set, skipping Slack status");
+                return
+            }
+        case 'cancelled':
+            color = "#5c5c5c"; // gray
+            message = core.getInput("cancelled-message");
+            if (message == "") {
+                core.warning("No cancelled message set, skipping Slack status");
+                return
+            }
+        case 'skipped':
+            color = "#a18d29"; // yellow
+            message = core.getInput("skipped-message");
+            if (message == "") {
+                core.warning("No skipped message set, skipping Slack status");
+                return
+            }
+        default:
+            core.error(`Unsupported status '${status}', please check inputs`)
+            return
+    }
+    return {
+        "attachments": [
+            {
+                "title": `Workflow Status`,
+                "mrkdwn_in": ["text"],
+                "color": color,
+                "text": `*Repository:* \`${context.repo.owner}/${context.repo.repo}\`\n*Workflow:* ${context.workflow}\n\n${message}\n\n<${workflowURL}|View Workflow>`,
+            }
+        ]
+    };
+}
